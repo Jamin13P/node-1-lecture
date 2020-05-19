bb69fbe6fbc101760d39c9bfbfb613bf76d5e70b
Jamin13P <benjaminthomsen1215@yahoo.com>
2020-05-19 10:52:33 -0700

    Finished node lecture

--- server/controller.js
+++ server/controller.js
@@ -1,4 +1,6 @@
-const users = require('../data.json')
+const users = require("../data.json")
+
+let id = users[users.length - 1].id + 1
 
 module.exports = {
   getAllUsers: (req, res) => {
@@ -16,13 +18,62 @@ module.exports = {
 
   getUserById: (req, res) => {
     // req.params.user_id
-    const { user_id } = req.params
+    const {user_id} = req.params
     const user = users.find((element) => element.id === +user_id)
 
     if (user) {
       res.status(200).send(user)
     } else {
-      res.status(404).send('User not found')
+      res.status(404).send("User not found")
     }
   },
+
+  createUser: (req, res) => {
+    const {first_name, last_name, email} = req.body
+    const newUser = {id, first_name, last_name, email}
+
+    users.push(newUser)
+
+    id++
+
+    res.status(200).send(users)
+  },
+
+  editUser: (req, res) => {
+    const {user_id} = req.params
+    const {first_name, last_name, email} = req.body
+
+    const index = users.findIndex(elem => elem.id === +user_id)
+
+    if(index === -1){
+      return res.status(404).send("User not found")
+    }
+
+    // const updatedUser = {id: +user_id, first_name, last_name, email}
+
+    const updatedUser = {
+      id: +user_id,
+      first_name: first_name || users[index].first_name,
+      last_name: last_name || users[index].last_name,
+      email: email || users[index].email
+    }
+
+    users[index] = updatedUser
+
+    res.status(200).send(users)
+  },
+
+  deleteUser: (req, res) => {
+    const {user_id} = req.params
+
+    const index = users.findIndex(elem => elem.id === +user_id)
+
+    if(index === -1){
+      return res.status(404).send("User not found")
+    }
+
+    users.splice(index, 1)
+
+    res.status(200).send(users)
+  }
 }
--- server/index.js
+++ server/index.js
@@ -1,17 +1,25 @@
-const express = require('express')
+const express = require("express")
 const app = express()
-const ctrl = require('./controller')
+const ctrl = require("./controller")
 const SERVER_PORT = 4000
 
 app.use(express.json())
 
-app.get('/api/andrew', (req, res) => {
+app.use(function(req, res, next) {
+  console.log("We got a request cap'n")
+  next()
+})
+
+app.get("/api/andrew", (req, res) => {
   console.log('I GOT A REQUEST A /api/andrew')
   res.status(200).send('Andrew')
 })
 
-app.get('/api/users', ctrl.getAllUsers)
-app.get('/api/users/:user_id', ctrl.getUserById)
+app.get("/api/users", ctrl.getAllUsers)
+app.get("/api/users/:user_id", ctrl.getUserById)
+app.post("/api/users", ctrl.createUser)
+app.put("/api/users/:user_id", ctrl.editUser)
+app.delete("/api/users/:user_id", ctrl.deleteUser)
 
 app.listen(SERVER_PORT, () =>
   console.log(`Server running on port ${SERVER_PORT}`)
